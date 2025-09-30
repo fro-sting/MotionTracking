c7cc5cc6bd7e1bac2b6e574a634ff27c89164542
liucao <l-cao20@mails.tsinghua.edu.cn>
2025-09-30 23:03:41 +0800

    clean action manager

--- active_adaptation/envs/mdp/action.py
+++ active_adaptation/envs/mdp/action.py
@@ -14,6 +14,7 @@ from active_adaptation.utils.math import (
     yaw_quat,
 )
 import active_adaptation.utils.symmetry as symmetry_utils
+from active_adaptation.assets.humanoid import G1_ACTION_SCALE
 
 if TYPE_CHECKING:
     from isaaclab.assets import Articulation
@@ -46,29 +47,6 @@ class ActionManager:
     @property
     def device(self):
         return self.env.device
-
-
-class ConcatenatedAction(ActionManager):
-    def __init__(self, env, action_managers: List[ActionManager]):
-        super().__init__(env)
-        self.action_managers: List[ActionManager] = action_managers
-        self.action_dims = [action_manager.action_dim for action_manager in self.action_managers]
-        self.action_dim = sum(self.action_dims)
-    
-    @property
-    def action_buf(self):
-        return torch.cat([action_manager.action_buf for action_manager in self.action_managers], dim=1)
-
-    def apply_action(self, tensordict: TENSORLIKE, substep: int):
-        actions = torch.split(tensordict["action"], self.action_dims, dim=-1)
-        for action_manager, action in zip(self.action_managers, actions):
-            action_manager.apply_action(action, substep)
-    
-    def symmetry_transforms(self):
-        return symmetry_utils.SymmetryTransform.cat(
-            [action_manager.symmetry_transforms() for action_manager in self.action_managers]
-        )
-
 
 class JointPosition(ActionManager):
     def __init__(
@@ -150,87 +128,6 @@ class JointPosition(ActionManager):
         pos_target = self.default_joint_pos.clone()
         pos_target[:, self.joint_ids] += self.applied_action * self.action_scaling
         self.asset.set_joint_position_target(pos_target)
-
-
-class LegWheel(ActionManager):
-    def __init__(
-        self,
-        env,
-        leg_scaling: Dict[str, float],
-        wheel_scaling: Dict[str, float]
-    ):
-        super().__init__(env)
-        self.leg_ids, self.leg_names, self.leg_scaling = (
-            string_utils.resolve_matching_names_values(
-                dict(leg_scaling), self.asset.joint_names
-            )
-        )
-        self.wheel_ids, self.wheel_names, self.wheel_scaling = (
-            string_utils.resolve_matching_names_values(
-                dict(wheel_scaling), self.asset.joint_names
-            )
-        )
-        self.leg_scaling = torch.tensor(self.leg_scaling, device=self.device)
-        self.wheel_scaling = torch.tensor(self.wheel_scaling, device=self.device)
-
-        self.leg_action_dim = len(self.leg_ids)
-        self.wheel_action_dim = len(self.wheel_ids)
-        self.action_dim = len(self.leg_ids) + len(self.wheel_ids)
-        
-        self.applied_action = torch.zeros(self.num_envs, self.action_dim, device=self.device)
-        self.action_buf = torch.zeros(self.num_envs, self.action_dim, 4, device=self.device)
-
-    def apply_action(self, tensordict: TensorDictBase, substep: int):
-        action = tensordict["action"]
-        if substep == 0:
-            self.action_buf = self.action_buf.roll(1, dims=-1)
-            self.action_buf[:, :, 0] = action
-        self.applied_action = self.applied_action.lerp(action, 0.8)
-        leg_action, wheel_action = self.applied_action.split([self.leg_action_dim, self.wheel_action_dim], dim=-1)
-        leg_pos_target = self.asset.data.default_joint_pos[:, self.leg_ids] + self.leg_scaling * leg_action
-        self.asset.set_joint_position_target(leg_pos_target, self.leg_ids)
-        wheel_vel_target = self.wheel_scaling * wheel_action
-        self.asset.set_joint_velocity_target(wheel_vel_target, self.wheel_ids)
-
-    def symmetry_transforms(self):
-        return symmetry_utils.SymmetryTransform.cat([
-            symmetry_utils.joint_space_symmetry(self.asset, self.leg_names),
-            symmetry_utils.joint_space_symmetry(self.asset, self.wheel_names),
-        ])
-
-
-class DecapAction(ActionManager):
-    """
-    Decaying Action Prior as described in https://arxiv.org/pdf/2310.05714
-    """
-    def __init__(self, env, action_scaling: Dict[str, float] = 8.0):
-        super().__init__(env)
-        self.joint_ids, self.joint_names, self.action_scaling = (
-            string_utils.resolve_matching_names_values(
-                dict(action_scaling), self.asset.joint_names
-            )
-        )
-        self.action_scaling = torch.tensor(self.action_scaling, device=self.device)
-        self.action_dim = len(self.joint_ids)
-
-        self.action_buf = torch.zeros(self.num_envs, self.action_dim, 4, device=self.device)
-        self.applied_action = torch.zeros(self.num_envs, self.action_dim, device=self.device)
-    
-    def symmetry_transforms(self):
-        transform = symmetry_utils.joint_space_symmetry(self.asset, self.joint_names)
-        return transform
-    
-    def apply_action(self, tensordict: TensorDictBase, substep: int):
-        if substep == 0:
-            if isinstance(tensordict, TensorDictBase):
-                action = tensordict["action"]
-            action = action.clamp(-10, 10)
-            self.action_buf[:, :, 1:] = self.action_buf[:, :, :-1]
-            self.action_buf[:, :, 0] = action
-            self.applied_action = self.applied_action.lerp(action, 0.8)
-        torque = self.applied_action * self.action_scaling
-        self.asset.set_joint_effort_target(torque, self.joint_ids)
-    
 
 def clamp_norm(x: torch.Tensor, max_norm: float):
     norm = x.norm(dim=-1, keepdim=True)
